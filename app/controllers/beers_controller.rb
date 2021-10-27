e0460cba163eec329b6a5b1a8fcb6c1b13236944
Carlos Henrique Moreira <carlos.h.moreira@ibge.gov.br>
2021-10-27 14:23:52 -0300

    Added new and create methods in beers controller and a view to new action.

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,3 +1,18 @@
 class BeersController < ApplicationController
-  
+  def new
+    @beer = Beer.new
+    authorize @beer
+  end
+
+  def create
+    @beer = Beer.new(beer_params)
+    @beer.user = current_user
+    authorize @beer
+
+    if @beer.save
+      redirect_to @beer, notice: 'Beer was successfully created into the catalog.'
+    else
+      render :new
+    end
+  end
 end
