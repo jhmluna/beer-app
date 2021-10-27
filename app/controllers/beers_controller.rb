d057b0f6cd32df5781210b4719b0d6a2eca8baad
tiago saraiva <tiagofsaraiva@hotmail.com>
2021-10-27 11:52:20 -0300

    made destroy controller

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,3 +1,19 @@
 class BeersController < ApplicationController
-  
+  before_action :set_beer, only: %i[show edit update destroy]
+
+  def destroy
+    @beer.destroy
+    redirect_to beers_url, notice: 'Beer has been removed.'
+  end
+
+  private
+
+  def set_beer
+    @beer = Beer.find(params[:id])
+    authorize @beer
+  end
+
+  def beer_params
+    params.require(:beer).permit(:name, :category, :price, :description)
+  end
 end
