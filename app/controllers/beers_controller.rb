3a4e349126eeddd753da22b671e518be6958438d
Augusto Mendonca <augusto.sousa@economia.gov.br>
2021-10-27 12:33:15 -0300

    insere index e show no controller e view e corrije rota

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,3 +1,12 @@
 class BeersController < ApplicationController
-  
+
+
+  def index
+    @beers = policy_scope(Beer)
+  end
+    
+  def show
+    # set_beer - Substituido pelo before_action
+  end
+
 end
