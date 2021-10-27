8daa103a2cd4f4c3ff90b5fbd138d2dfce4be2ab
Henrique Luna <jhmluna@gmail.com>
2021-10-27 16:36:34 -0300

    User show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
+
+  def show
+    # set_user - Substituido pelo before_action
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+    authorize @user
+  end
 end
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,17 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    owner_or_admin?
+  end
+
+  private
+
+  def owner_or_admin?
+    record == user || user.admin
+  end
+end
