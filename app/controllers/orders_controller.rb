18adcce8aeee3461e170debe9a995936832c8b64
tiago saraiva <tiagofsaraiva@hotmail.com>
2021-10-27 18:08:07 -0300

    add sale and order index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,6 @@
 class OrdersController < ApplicationController
+
+  def index
+    @orders = Order.where(user_id: current_user)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+
+  def sale
+    @sales = Order.beer.where(user_id: current_user)
+  end
 end
