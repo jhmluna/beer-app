a8e27fd422e93acd6cfff108561bb4f659f13468
Augusto Mendonca <augusto.sousa@economia.gov.br>
2021-10-27 17:46:33 -0300

    inclui orders - controller e view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,23 @@
 class OrdersController < ApplicationController
+   def new
+      @order = Order.new
+      authorize @order
+   end
+    
+   def create
+     @order = Order.new(order_params)
+     @order.user = current_user
+     authorize @order
+  
+     if @order.save
+       redirect_to @order, notice: 'Order was successfully created into the catalog.'
+     else
+       render :new
+     end
+   end
+
+   def order_params
+    params.require(:beer).permit(:date)
+  end
+
 end
