bf7beac993dcb91bcdc09feec0a53b9187dd8f4d
Henrique Luna <jhmluna@gmail.com>
2021-10-28 09:04:56 -0300

    Improve box shadow

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,10 +27,8 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-import { initUpdateCardOnHover } from '../components/card';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  initUpdateCardOnHover();
 });
